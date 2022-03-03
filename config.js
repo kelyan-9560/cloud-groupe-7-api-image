9d78f71a012d18e7a6d4bfda8eee57f03b375137
kelyan-9560 <kelyan.bervin@gmail.com>
2022-03-03 23:01:19 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'USJ8NJSYI46G20IHNGRS',
+    secretAccessKey: 'TM93UidKwT3ElUUUnzntencmeOQ7dVAzn6UOGZde',
+    endpoint: 's3.eu-west-1.wasabisys.com',
+    bucket: 'cloud-groupe-7-api',
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
