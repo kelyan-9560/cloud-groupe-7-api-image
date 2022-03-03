ab0884691d85c537861d2d55eb96658ac2e2171f
kelyan-9560 <kelyan.bervin@gmail.com>
2022-03-03 23:06:07 +0100

    refacto

--- config.js
+++ config.js
@@ -4,9 +4,5 @@ module.exports = {
     secretAccessKey: 'TM93UidKwT3ElUUUnzntencmeOQ7dVAzn6UOGZde',
     endpoint: 's3.eu-west-1.wasabisys.com',
     bucket: 'cloud-groupe-7-api',
-  },
-  suffix: {
-    small: '_small.jpg',
-    full: '_full.jpg'
   }
 };
